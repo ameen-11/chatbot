c72b1f3efcdeaac4b9eb927f3e00bf5446b400f2
ameen-11 <bangaloreameen@gmail.com>
2024-06-06 05:37:22 +0530

    basic flex layout implemented

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,3 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [
-    { path: '', redirectTo: '/home', pathMatch: 'full' }, // Redirect to the 'home' route
-    { path: 'home', component: HomeComponent },
-  ];
-  
+export const routes: Routes = [];
